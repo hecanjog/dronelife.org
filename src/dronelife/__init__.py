1f23fee8266fbaa32b486c6a059ff5caaf9ce46b
Erik Schoster <erik@heythere.org>
2014-02-15 11:32:03 -0600

    Remove lesscss deps and configure foundation. Closes #6

--- src/dronelife/__init__.py
+++ src/dronelife/__init__.py
@@ -7,11 +7,6 @@ app = Flask(__name__)
 # Load app config info from environment variables
 from_envvars(app.config, prefix=app.name.upper() + '_')
 
-# Compile less files into css
-from flaskext.lesscss import lesscss
-app.static_path = app.static_url_path # the version of flask-lesscss in PyPI is outdated
-lesscss(app)
-
 db = SQLAlchemy(app)
 
 from dronelife import controller
