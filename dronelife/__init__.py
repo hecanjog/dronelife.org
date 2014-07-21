25552dbb7f30198baff2a9ff739f80d0e4e9b905
Erik Schoster <erik@hecanjog.com>
2014-07-21 18:54:52 -0500

    add basic logging..."

--- dronelife/__init__.py
+++ dronelife/__init__.py
@@ -3,6 +3,8 @@ from flask_appconfig.env import from_envvars
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager 
+import os
+import logging
 
 login_manager = LoginManager()
 
@@ -14,5 +16,9 @@ from_envvars(app.config, prefix=app.name.upper() + '_')
 login_manager.init_app(app)
 
 db = SQLAlchemy(app)
+
+log_handler = logging.FileHandler(os.path.expanduser('~/dronelife.flask.log'))
+log_handler.setLevel(logging.WARNING)
+app.logger.addHandler(log_handler)
 
 from dronelife import controller
